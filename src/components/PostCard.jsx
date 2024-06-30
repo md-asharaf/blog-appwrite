80fbfaafd90c7896c6bb07c4c7b06bbd1e61b34f
md asharaf <718romanempire@gmail.com>
2024-06-30 12:47:05 +0530

    chore: Update PostCard image width
    The PostCard component now sets a fixed width for the image, improving layout consistency.
    
    ```

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -6,7 +6,7 @@ function PostCard({ $id, title, imageUrl }) {
             <div className="w-full bg-gray-100 rounded-xl p-4 border-[1.5px] border-black">
                 <div className="w-full justify-center mb-4">
                     <img
-                        className="rounded-xl w-56 h-32"
+                        className="rounded-xl w-56"
                         src={postService.getFilePreview(imageUrl)}
                         alt={title}
                     />
